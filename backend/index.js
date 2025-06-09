7a036537563da2734311b76cd3fc52ea5b5aef24
Kirti Bhosale <bhosalekirti2002@gmail.com>
2025-06-09 16:20:24 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://skai-lama-assignment-ivory.vercel.app",
-    ],
+    origin: "*",
     credentials: true,
 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
