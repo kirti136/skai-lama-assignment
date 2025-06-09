c4f85c3ad0623f463e17307f10589fec7c37613a
Kirti Bhosale <bhosalekirti2002@gmail.com>
2025-06-09 16:54:17 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(
     credentials: true,
 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
