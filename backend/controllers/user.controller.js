7257f7cd937a29e790f05598b95b8f5161eb4c68
Kirti Bhosale <bhosalekirti2002@gmail.com>
2025-06-09 12:30:34 +0530

    expiry time updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -68,7 +68,7 @@ exports.login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, email: user.email },
       process.env.JWT_SECRET_KEY,
-      { expiresIn: "1h" }
+      { expiresIn: "7d" }
     );
 
     // Set cookie
